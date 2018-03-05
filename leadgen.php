c11f3ac8076ddc0d124a32a08e46b46a027087e2
Leighton <leigh@incsub.com>
2018-03-05 17:21:12 -0500

    [New] Plugin file

---
+++ leadgen.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:	Lead Gen Form
+ * Description:	Collect customer data and build customer profiles inside of your WordPress Dashboard.
+ * Version:		1.0
+ * Author:		Leighton Sapir
+ * Author URI:	http://iamleigh.com
+ */
+
+defined( 'ABSPATH' ) or die( "Now you see me, now you don't... puff!!!" );
