9d91857add50353a0813bcb8f6eba30f08cb7e15
Leighton <leighsapir@icloud.com>
2018-03-05 22:49:08 -0500

    [Update] Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
 			},
 			target: {
 				files: {
-					'assets/css/leadgen.min.css': ['assets/css/leadgen.css']
+					'assets/css/leadgen.css': ['assets/css/leadgen.css']
 				}
 			}
 		},
